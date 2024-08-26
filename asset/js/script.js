09f1dee75ab8599ff85500fc7b52862c6c98d499
Exoduscode <134938281+Exoduscode@users.noreply.github.com>
2024-08-26 18:33:46 +0300

    Add files via upload

---
+++ asset/js/script.js
@@ -0,0 +1,17 @@
+//----Auto type JS from above home section----
+var typed=new Typed(".auto-type",
+    {
+      strings: ["Software Engineer","Web Developer","Programmer","Coder"],
+      typeSpeed: 150,
+      backSpeed: 150,
+      loop: true
+    })
+    //------------Auto_type_For_Hero_End---------------------
+    
+    //---------Autoplay Slide Start----------
+  var myCarousel = document.querySelector('#carouselExampleCaptions');
+  var carousel = new bootstrap.Carousel(myCarousel, {
+      interval: 3000, // Slide interval in milliseconds
+      ride: 'carousel' // Ensures the carousel auto-slides
+  });
+  //---------Autoplay Slide End
